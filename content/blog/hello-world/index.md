ea1c2f49c427dfb6141c4b1a87e4daf5582a3182
Rahul Mistri <58909377+rahulmistri1997@users.noreply.github.com>
2024-01-09 21:07:40 +0530

    add: Base Code

---
+++ content/blog/hello-world/index.md
@@ -0,0 +1,8 @@
+---
+title: Hello World
+date: "2024-01-01T00:00:00.000Z"
+description: "Hello World"
+---
+
+This is the first post on my new blog. I’m just getting this new blog going, i do not have much to say right now but i am getting things ready for the future.
+
---
+++ src/components/seo.js
@@ -0,0 +1,50 @@
+/**
+ * SEO component that queries for data with
+ * Gatsby's useStaticQuery React hook
+ *
+ * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
+ */
+
+import * as React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+const Seo = ({ description, title, children }) => {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            social {
+              linkedin
+            }
+          }
+        }
+      }
+    `
+  )
+
+  const metaDescription = description || site.siteMetadata.description
+  const defaultTitle = site.siteMetadata?.title
+
+  return (
+    <>
+      <title>{defaultTitle ? `${title} | ${defaultTitle}` : title}</title>
+      <meta name="description" content={metaDescription} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={metaDescription} />
+      <meta property="og:type" content="website" />
+      <meta name="linkedin:card" content="summary" />
+      <meta
+        name="linkedin:creator"
+        content={site.siteMetadata?.social?.linkedin || ``}
+      />
+      <meta name="linkedin:title" content={title} />
+      <meta name="linkedin:description" content={metaDescription} />
+      {children}
+    </>
+  )
+}
+
+export default Seo
